202fcf0d2738111c7c1dc0d8bf7b7dfcfbebe292
gopae02 <gopae02@gmail.com>
2021-04-02 23:13:19 +0900

    # 2-01 주소와 화면을 연결하는 URL과 뷰
    https://wikidocs.net/70649

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 # 2021-04-01 파이썬 장고 
   - https://wikidocs.net/72377
   - 개발서버 구동하기 :    (mysite) C:\projects\mysite>   python manage.py runserver
+
+# 2021-04-02 장고 기본 앱 생성 
+  - django-admin의 startapp 명령을 이용하여 pybo 앱을 생성
+  > (mysite) C:\projects\mysite>django-admin startapp pybo
+
--- config/urls.py
+++ config/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# 장고가 사용자의 페이지 요청을 이해할 수 있도록 config/urls.py 파일
+# config/urls.py 파일은 페이지 요청 시 가장 먼저 호출되며, 요청 URL과 뷰 함수를 1:1로 연결"
+
 from django.contrib import admin
-from django.urls import path
+# ---------------------------------- [edit] ---------------------------------- #
+from django.urls import path, include
+# ---------------------------------------------------------------------------- #
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+# ---------------------------------- [edit] ---------------------------------- #
+    path('pybo/', include('pybo.urls')),
+# ---------------------------------------------------------------------------- #
 ]
