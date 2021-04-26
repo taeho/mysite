92fa8e5eca48a79efb47cd0fc28792981e68acf8
gopae02 <gopae02@gmail.com>
2021-04-26 15:46:59 +0900

    # 2-05 URL 더 똑똑하게 사용하기 https://wikidocs.net/70741

--- pybo/urls.py
+++ pybo/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    # /pybo/2/의 숨은 의도는 'Question 모델 데이터 중 id값이 2인 데이터를 조회하라'이다
-    path('<int:question_id>/', views.detail),
+    # 템플릿의 href에 실제 주소가 아니라 URL 별칭을 사용
+    # pybo/urls.py 파일에 네임스페이스를 추가하려면 간단히 app_name 변수에 네임스페이스 이름을 저장
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
 ]
